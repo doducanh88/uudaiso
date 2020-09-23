221f6b6054487fc35ebd96218c5e0e8c0d352133
The Anh <theanhtran135@gmail.com>
2020-09-23 13:39:40 +0700

    23/9

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
-    $('.sidenav').sidenav();
     $('.carousel.carousel-slider').carousel({
         fullWidth: true,
         indicators: true,
         duration: 100
     });
+    $('.sidenav').sidenav();
+});
+
+var app = angular.module('uudaiso', []);
+app.controller('uudaisoCtrl', function($scope, $http) {
+    $scope.add = 0;
+    $scope.openFunc = function() {
+        if (add % 2 == 0) {
+            $scope.result = true;
+        } else {
+            $scope.result = false
+        }
+        $scope.add++;
+    };
 });
